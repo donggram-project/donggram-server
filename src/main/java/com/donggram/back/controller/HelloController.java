276b9a7779c6244973a14d8a570a028483375178
Changha-dev <68684425+Changha-dev@users.noreply.github.com>
2023-07-11 14:21:56 +0900

    first commit

---
+++ src/main/java/com/donggram/back/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.donggram.back.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @PostMapping("/hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("hello!");
+
+    }
+}
