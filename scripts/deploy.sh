3f3aa893db8fe2704e02323381afaf2824251537
changha <mentenseoul@gmail.com>
2023-08-31 13:14:05 +0900

    deploy.sh test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,6 +18,8 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
+## ..
+
 JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 
 echo "> JAR NAME: $JAR_NAME"
@@ -28,4 +30,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1
